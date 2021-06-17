7bf58054e90f0df1590f7bbce2d10a61690cc6de
Dominique De León <dominique.a.deleon@gmail.com>
2021-06-17 16:36:49 +0000

    Automatically backed up by Learn

---
+++ bartender/environment.rb
@@ -0,0 +1,7 @@
+require "pry"
+require "httparty" ## let me make an outside request to an api
+require "json" ## lets eme parse json data
+
+require_relative "./bartender/api"
+require_relative "./bartender/cli"
+require_relative "./bartender/drink"
